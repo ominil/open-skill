100cc06b4be548f80b73309fe2538c873d009d28
Luca Omini <lucaomini751@gmail.com>
2022-10-21 21:08:11 +0200

    Spring boot app with demo controller

---
+++ src/main/java/com/kleinmann/openskill/OpenskillApplication.java
@@ -0,0 +1,13 @@
+package com.kleinmann.openskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenskillApplication.class, args);
+	}
+
+}
